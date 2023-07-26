d09952df2d86f6294fdf8736d097da3e034f46c7
tmendoza27 <trevormendoza2002@gmail.com>
2023-07-26 10:56:26 -0700

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { puppyList } from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppies);
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
