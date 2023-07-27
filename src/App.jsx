747d54fd4927cc14d6ae7dc6d89994b6a79ddc9b
tmendoza27 <trevormendoza2002@gmail.com>
2023-07-26 17:06:03 -0700

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,26 @@ import './App.css'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
-
+  const [featPupId, setFeatPupId] = useState(null)
   console.log("puppyList: ", puppies);
-
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <div className="App">
       {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
+             return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
            })
        }
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
